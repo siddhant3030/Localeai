fe03abc5f912cb80af809a91766ba858ae393e75
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-07 14:15:42 +0530

    user model added

---
+++ lib/localai/model/user.ex
@@ -0,0 +1,27 @@
+defmodule Localai.Model.User do
+  alias Localai.Repo
+  alias Localai.Schema.User
+  import Ecto.Query
+
+  def list_users() do
+    Repo.all(Role)
+  end
+
+  def get_user!(id), do: Repo.get!(User, id)
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
+  end
+end
